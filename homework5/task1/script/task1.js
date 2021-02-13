578639cac5b1c58308059744ceec9541799ffb65
Николай Гаркушенко <nekdfl@gmail.com>
2021-02-13 17:58:39 +0300

    task 1 chessboard

--- homework5/task1/script/task1.js
+++ homework5/task1/script/task1.js
@@ -1,28 +1,73 @@
-'use strict';
-
-const settings = {
-    temp: "temp"
-};
-
+"use strict";
 
 
 const chessfield = {
-    settings,
+    chessboardEl: document.getElementById('chessboard'),
+    row_arr: [0, 8, 7, 6, 5, 4, 3, 2, 1, 0],
+    column_arr: ['x', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'x'],
 
+    isEven(n) {
+        if (n % 2 === 0)
+            return true;
 
-    run() {
-        this.init();
-        this.render();
+        return false;
     },
 
-    init() {
+    addNumberedRow(rowNum) {
+        let currentFieldColorBlack = true;
+        if (!this.isEven(rowNum)) {
+            currentFieldColorBlack = false;
+        }
+
+        const tr = document.createElement('tr');
+        this.chessboardEl.appendChild(tr);
+
+        for (let col = 0; col < this.column_arr.length; col++) {
+            const td = document.createElement('td');
+            tr.appendChild(td);
+
+            //внешние цифровые надписи
+            if (col === 0 || col === 9)
+                td.innerHTML = this.row_arr[rowNum];
+            //иначе игровое поле
+            else if (currentFieldColorBlack) {
+                td.style.backgroundColor = '#732e11';
+                currentFieldColorBlack = false;
+            } else {
+                td.style.backgroundColor = '#FaFaEA';
+                currentFieldColorBlack = true;
+            }
+        }
 
     },
 
-    render() {
+    addAlaphabetRow() {
+        const tr = document.createElement('tr');
+        this.chessboardEl.appendChild(tr);
 
+        for (let col = 0; col < this.column_arr.length; col++) {
+
+            const td = document.createElement('td');
+
+            if (col > 0 && col < 9)
+                td.innerHTML = this.column_arr[col];
+
+            tr.appendChild(td);
+        }
+
+
+    },
+
+    renderField() {
+        for (let row = 0; row < this.row_arr.length; row++) {
+            if (this.row_arr[row] === 0)
+                this.addAlaphabetRow();
+            else
+                this.addNumberedRow(row);
+        }
     },
 
 };
 
-chessfield.run();
+
+chessfield.renderField();
